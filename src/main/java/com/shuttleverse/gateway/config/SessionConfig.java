52760452707c2684d8f037e27425f3abb82fbc67
QW66666 <wusteven0829@gmail.com>
2025-04-19 06:08:10 -0400

    feat: redis session configuration

--- src/main/java/com/shuttleverse/gateway/config/SessionConfig.java
+++ src/main/java/com/shuttleverse/gateway/config/SessionConfig.java
@@ -1,18 +1,44 @@
 package com.shuttleverse.gateway.config;
 
+import java.time.Duration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
+import org.springframework.data.redis.core.ReactiveRedisOperations;
+import org.springframework.data.redis.core.ReactiveRedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.session.data.redis.config.annotation.web.server.EnableRedisWebSession;
 import org.springframework.web.server.session.CookieWebSessionIdResolver;
 import org.springframework.web.server.session.WebSessionIdResolver;
 
 @Configuration
+@EnableRedisWebSession()
 public class SessionConfig {
+
+  @Bean
+  public ReactiveRedisOperations<String, Object> redisOperations(
+      ReactiveRedisConnectionFactory factory) {
+    Jackson2JsonRedisSerializer<Object> serializer = new Jackson2JsonRedisSerializer<>(
+        Object.class);
+
+    RedisSerializationContext.RedisSerializationContextBuilder<String, Object> builder =
+        RedisSerializationContext.newSerializationContext(new StringRedisSerializer());
+
+    RedisSerializationContext<String, Object> context = builder
+        .value(serializer)
+        .hashValue(serializer)
+        .build();
+
+    return new ReactiveRedisTemplate<>(factory, context);
+  }
 
   @Bean
   public WebSessionIdResolver webSessionIdResolver() {
     CookieWebSessionIdResolver resolver = new CookieWebSessionIdResolver();
     resolver.setCookieName("GATEWAY_SESSION");
-    resolver.setCookieMaxAge(java.time.Duration.ofMinutes(30));
+    resolver.setCookieMaxAge(Duration.ofMinutes(30));
     resolver.addCookieInitializer(responseCookieBuilder ->
         responseCookieBuilder
             .path("/")
